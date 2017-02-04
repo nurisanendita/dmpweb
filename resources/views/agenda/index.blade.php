484b634ddca0e0b5d376dd70460ba71c47c82727
nuritharra <nuri.tharra.s@mail.ugm.ac.id>
2017-02-04 21:59:33 +0700

    v

--- resources/views/agenda/index.blade.php
+++ resources/views/agenda/index.blade.php
@@ -13,7 +13,7 @@
 <div class="box">
 	<div class"box-header" style="margin-top:10px;">
 		<div class="col-sm-2">
-			<a class="btn btn-success" href="{{ url('/') }}/agenda_create">TAMBAH</a>
+			<a class="btn btn-success" href="{{ url('/') }}/agenda_create" style="float: right;">TAMBAH</a>
 		</div>
 	</div>
 	<div class="box-body">
