e2edb5a9abb7e18cc58f932c344e1c192b064992
nuritharra <nuri.tharra.s@mail.ugm.ac.id>
2017-02-06 00:17:35 +0700

    p

--- app/Http/Controllers/DokumentasiController.php
+++ app/Http/Controllers/DokumentasiController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Redirect;
