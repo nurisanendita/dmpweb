4109a69b034c53697161b8b281023b33e76c7f7e
nuritharra <nuri.tharra.s@mail.ugm.ac.id>
2017-02-05 14:25:53 +0700

    p

--- app/Http/Controllers/AgendaController.php
+++ app/Http/Controllers/AgendaController.php
@@ -56,7 +56,7 @@ class AgendaController extends Controller
 
     public function show()
     {
-      $agenda = Agenda::all();
+      $agenda = DB::table('agenda')->where('ditampilkan', '=', 1)->select();
       return view('welcome', compact('agenda'));
     }
 
