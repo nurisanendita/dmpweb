ec36e06832b5e05f0e203b5b318841a71cac6703
nuritharra <nuri.tharra.s@mail.ugm.ac.id>
2017-02-05 14:18:25 +0700

    p

--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,7 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'AgendaController@show');
 
 Route::get('/dokumentasi', function () {
     return view('dokumentasi.dokumentasi');
