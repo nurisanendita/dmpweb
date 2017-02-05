49465a291fef91e1da0077e6eb9a26f86280a787
nuritharra <nuri.tharra.s@mail.ugm.ac.id>
2017-02-05 20:38:57 +0700

    p

--- app/Http/Controllers/QuoteController.php
+++ app/Http/Controllers/QuoteController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Redirect;
