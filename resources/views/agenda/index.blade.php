3f6b034d00411c91689e251e50b82568b382faf2
nuritharra <nuri.tharra.s@mail.ugm.ac.id>
2017-02-05 11:32:30 +0700

    commit

--- resources/views/agenda/index.blade.php
+++ resources/views/agenda/index.blade.php
@@ -1,7 +1,7 @@
 @extends('admin.admin_template')
 
 @section('content')
-@if($show == 12)
+@if($show > 12)
 <script type='text/javascript'>alert('Agenda Ditampilkan 12')</script>
 @endif
 <section class="content-header">
