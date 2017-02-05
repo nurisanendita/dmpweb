25ea1dab2add73ba3ebcc2aac8ef2c7b6a4e2d90
nuritharra <nuri.tharra.s@mail.ugm.ac.id>
2017-02-05 23:02:45 +0700

    p

--- app/Http/Controllers/AgendaController.php
+++ app/Http/Controllers/AgendaController.php
@@ -59,7 +59,7 @@ class AgendaController extends Controller
 
     public function show()
     {
-      if($agenda = Agenda::where('id','=','0')->get()
+      if($agenda = Agenda::where('id','=','0')->get())
       {
         Session::flash('message', 'Belum Melakukan Input Agenda');
         return redirect('/');
