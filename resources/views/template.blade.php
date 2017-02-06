e08f8c2fb8db5ba4286d2c7207b1e1252e9b3d3a
nuritharra <nuri.tharra.s@mail.ugm.ac.id>
2017-02-06 18:42:57 +0700

    commit

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -53,5 +53,15 @@
             </div>
         </nav>
         @yield('content')
+        <script>
+        $(function() {
+             var pgurl = window.location.href.substr(window.location.href
+        .lastIndexOf("/")+1);
+             $("#nav ul li a").each(function(){
+                  if($(this).attr("href") == pgurl || $(this).attr("href") == '' )
+                  $(this).addClass("active");
+             })
+        });
+        </script>
     </body>
 </html>
