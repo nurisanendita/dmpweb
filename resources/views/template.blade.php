8101805a10ab3c1b56a2437d5126b8e0cc7e39bc
nuritharra <nuri.tharra.s@mail.ugm.ac.id>
2017-02-06 18:54:28 +0700

    commit

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -57,7 +57,7 @@
         $(function() {
              var pgurl = window.location.href.substr(window.location.href
         .lastIndexOf("/")+1);
-             $("li").each(function(){
+             $(".nav .navbar-nav li a").each(function(){
                   if($(this).attr("href") == pgurl || $(this).attr("href") == '' )
                   $(this).addClass("active");
              })
