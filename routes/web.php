53a0f5ca41d435bbcc54edadf4c6a4536bd4687e
nuritharra <nuri.tharra.s@mail.ugm.ac.id>
2017-02-06 22:50:00 +0700

    commit

--- routes/web.php
+++ routes/web.php
@@ -10,21 +10,20 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', 'AgendaController@show');
-
-Route::get('/dokumentasi', 'DokumentasiController@show');
-
-Route::get('/berita', function () {return view('berita.berita');});
-
-Route::get('/tentang_kami', function () {return view('profil.profil');});
-
-Route::get('/kontak', function () {return view('kontak.kontak');});
-
-Route::get('/admin_dmp', function () {return view('admin.admin_login');});
-Route::post('/admin_dmpPost', 'AdminController@authenticate');
-
 Route::group(['middleware' => 'auth'], function () {
+  Route::get('/', 'AgendaController@show');
+
+  Route::get('/dokumentasi', 'DokumentasiController@show');
+
+  Route::get('/berita', function () {return view('berita.berita');});
+
+  Route::get('/tentang_kami', function () {return view('profil.profil');});
+
+  Route::get('/kontak', function () {return view('kontak.kontak');});
+
+  Route::get('/admin_dmp', function () {return view('admin.admin_login');});
+  Route::post('/admin_dmpPost', 'AdminController@authenticate');
+
   Route::get('/agenda_index', 'AgendaController@index');
   Route::get('/agenda_create', 'AgendaController@create');
   Route::post('/agenda_store', 'AgendaController@store');
