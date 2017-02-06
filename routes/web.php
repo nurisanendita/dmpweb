8731fbb112eab59428f518889587482e90f579c5
nuritharra <nuri.tharra.s@mail.ugm.ac.id>
2017-02-06 22:48:50 +0700

    commit

--- routes/web.php
+++ routes/web.php
@@ -24,22 +24,23 @@ Route::get('/kontak', function () {return view('kontak.kontak');});
 Route::get('/admin_dmp', function () {return view('admin.admin_login');});
 Route::post('/admin_dmpPost', 'AdminController@authenticate');
 
-Route::get('/agenda_index', 'AgendaController@index');
-Route::get('/agenda_create', 'AgendaController@create');
-Route::post('/agenda_store', 'AgendaController@store');
-Route::get('/agenda_edit/{id}', 'AgendaController@edit');
-Route::put('/agenda_update/{id}', 'AgendaController@update');
-// Route::get('/agenda_delete/{id}', 'AgendaController@destroy');
+Route::group(['middleware' => 'auth'], function () {
+  Route::get('/agenda_index', 'AgendaController@index');
+  Route::get('/agenda_create', 'AgendaController@create');
+  Route::post('/agenda_store', 'AgendaController@store');
+  Route::get('/agenda_edit/{id}', 'AgendaController@edit');
+  Route::put('/agenda_update/{id}', 'AgendaController@update');
+  // Route::get('/agenda_delete/{id}', 'AgendaController@destroy');
 
-Route::get('/dokumentasi_index', 'DokumentasiController@index');
-Route::get('/dokumentasi_create', 'DokumentasiController@create');
-Route::post('/dokumentasi_store', 'DokumentasiController@store');
-Route::get('/dokumentasi_edit/{id}', 'DokumentasiController@edit');
-Route::put('/dokumentasi_update/{id}', 'DokumentasiController@update');
-// Route::get('/dokumentasi_delete/{id}', 'AgendaController@destroy');
+  Route::get('/dokumentasi_index', 'DokumentasiController@index');
+  Route::get('/dokumentasi_create', 'DokumentasiController@create');
+  Route::post('/dokumentasi_store', 'DokumentasiController@store');
+  Route::get('/dokumentasi_edit/{id}', 'DokumentasiController@edit');
+  Route::put('/dokumentasi_update/{id}', 'DokumentasiController@update');
+  // Route::get('/dokumentasi_delete/{id}', 'AgendaController@destroy');
 
-Route::get('/quote_index', function () {return view('quote.index');});
-
+  Route::get('/quote_index', function () {return view('quote.index');});
+});
 // Auth::routes();
 //
 // Route::get('/admin_dmp', 'HomeController@index');
