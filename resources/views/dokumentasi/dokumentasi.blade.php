365ed12ebc8ee1b38a24eee23e390688b649db77
nuritharra <nuri.tharra.s@mail.ugm.ac.id>
2017-02-07 14:01:04 +0700

    commit

--- resources/views/dokumentasi/dokumentasi.blade.php
+++ resources/views/dokumentasi/dokumentasi.blade.php
@@ -11,7 +11,7 @@
         <div class="col-md-3 col-lg-3">
           <div class="info-box bg-orange" style="height:500px; font-family:Hind;">
             <div class="info-box-content" style="margin-left:30px; margin-right:30px;">
-              <span><h5 style="margin-top:30px; margin-bottom:30px;"><b>{{ $dokumentasi_list->judul_foto }}</b></h5></span><br>
+              <span><h5 style="margin-top:20px;"><b>{{ $dokumentasi_list->judul_foto }}</b></h5></span><br>
               <p>{{ $dokumentasi_list->keterangan_foto }}</p>
               <p>{{ $dokumentasi_list->tanggal }}</p>
             </div>
