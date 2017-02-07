7ba8ff0c29c757cdec166f4bc03b0af0d2b5c42e
nuritharra <nuri.tharra.s@mail.ugm.ac.id>
2017-02-07 07:36:50 +0700

    commit

--- resources/views/dokumentasi/dokumentasi.blade.php
+++ resources/views/dokumentasi/dokumentasi.blade.php
@@ -10,13 +10,13 @@
         <div class="col-md-3 col-lg-3">
           <div class="info-box bg-orange" style="height:500px; font-family:Hind;">
             <div class="info-box-content" style="margin-left:30px; margin-right:30px; font-size:18px;">
-              <h3 style="margin-top:30px; margin-bottom:30px;">haloooo</h3>
+              <h3 style="margin-top:30px; margin-bottom:30px;">{{$dokumentasi1->keterangan_foto}}</h3>
             </div>
           </div>
         </div>
 
         <div class="col-md-9 col-lg-9">
-          <img src="img_kaltim2.jpg" style="height:500px; width:850px;">
+          <img src="{{$dokumentasi1->foto}}" style="height:500px; width:850px;">
           <div class="row">
             <div class="container">
               <a class="w3-btn-floating w3-display-left w3-opacity-off" onclick="plusDivs(-1)" style="margin-left:20px; opacity: 0.5;">&#10094;</a>
