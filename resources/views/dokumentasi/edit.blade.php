b354b84ac08219f97355b89b63d33c4fb6f8e71a
nuritharra <nuri.tharra.s@mail.ugm.ac.id>
2017-02-07 14:15:44 +0700

    commit

--- resources/views/dokumentasi/edit.blade.php
+++ resources/views/dokumentasi/edit.blade.php
@@ -9,8 +9,14 @@
 	<b>Foto</b>
 	<input style="margin-left:10px;" type="file" name="input_foto" value="{{$dokumentasi->foto}}"><br><br>
 
-  <b>Keterangan Foto</b>
-	<textarea class="form-control" style="height:200px; width:700px;" type="text" name="input_keteranganfoto">{{$dokumentasi->keteranganfoto}}</textarea><br><br><br>
+	<b>Judul Foto</b>
+	<textarea class="form-control" style="height:50px; width:400px;" type="text" name="input_judulfoto">{{$dokumentasi->judul_foto}}</textarea><br><br><br>
+
+	<b>Tanggal Foto Foto</b>
+	<textarea class="form-control" style="height:50px; width:400px;" type="text" name="input_tanggalfoto">{{$dokumentasi->tanggal}}</textarea><br><br><br>
+
+	<b>Keterangan Foto</b>
+	<textarea class="form-control" style="height:200px; width:700px;" type="text" name="input_keteranganfoto">{{$dokumentasi->keterangan_foto}}</textarea><br><br><br>
 
 	<input class="btn btn-info" type="submit" name="submit" value="SIMPAN">
 	{{ csrf_field() }}
