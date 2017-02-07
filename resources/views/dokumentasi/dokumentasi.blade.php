20dfed75026204d5e8c1b67cebeba78f075313cc
nuritharra <nuri.tharra.s@mail.ugm.ac.id>
2017-02-07 20:04:20 +0700

    commit

--- resources/views/dokumentasi/dokumentasi.blade.php
+++ resources/views/dokumentasi/dokumentasi.blade.php
@@ -34,8 +34,7 @@
 
   <div class="row" style="margin-top:30px; font-family:Montserrat; font-size:20px;">
     <div class="container">
-      <span><marquee behavior="slide" direction="left" style="text-transform:uppercase;">running text</img></marquee><marquee behavior="scroll" direction="left" style="text-transform:uppercase;">running text</img></marquee></span>
-    </div>
+      <marquee behavior="scroll" direction="left" style="text-transform:uppercase;">running text</img></marquee>
   </div>
 
   <script>
