f48154b59378b52bc6554f9ae51d837f724004f1
nuritharra <nuri.tharra.s@mail.ugm.ac.id>
2017-02-07 09:56:37 +0700

    commit

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -41,21 +41,8 @@
         <h2>Administrator</h2>
     </div>
     <!-- /.login-logo -->
-    <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @if (Auth::check())
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ url('/login') }}">Login</a>
-                        <a href="{{ url('/register') }}">Register</a>
-                    @endif
-                </div>
-            @endif
-    </div>
     <div class="login-box-body">
-        <form role="form" method="POST" action="{{ route('login') }}">
-          {{ csrf_field() }}
+        <form>
             <div class="form-group has-feedback">
                 <input type="text" class="form-control" placeholder="Username" name="username" id="username">
                 <span class="glyphicon glyphicon-user form-control-feedback"></span>
