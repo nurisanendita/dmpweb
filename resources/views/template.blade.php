fc787bdd40c55d35d81b685b43c83aa7edb7b985
nuritharra <nuri.tharra.s@mail.ugm.ac.id>
2017-02-08 23:30:05 +0700

    commit

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -43,11 +43,11 @@
               </div>
               <div class="collapse navbar-collapse" id="myNavbar">
                 <ul class="nav navbar-nav">
-                  <li class="{{ Request::is('/') ? "active" : "" }}"><a href="{{ url('/') }}">BERANDA</a></li>
-                  <li class="{{ Request::is('berita') ? "active" : "" }}"><a href="{{ url('/') }}/berita">BERITA</a></li>
-                  <li class="{{ Request::is('dokumentasi') ? "active" : "" }}"><a href="{{ url('/') }}/dokumentasi">DOKUMENTASI</a></li>
-                  <li class="{{ Request::is('kontak') ? "active" : "" }}"><a href="{{ url('/') }}/kontak">KONTAK</a></li>
-                  <li class="{{ Request::is('tentang_kami') ? "active" : "" }}"><a href="{{ url('/') }}/tentang_kami">TENTANG KAMI</a></li>
+                  <li><a href="{{ url('/') }}">BERANDA</a></li>
+                  <li><a href="{{ url('/') }}/berita">BERITA</a></li>
+                  <li><a href="{{ url('/') }}/dokumentasi">DOKUMENTASI</a></li>
+                  <li><a href="{{ url('/') }}/kontak">KONTAK</a></li>
+                  <li><a href="{{ url('/') }}/tentang_kami">TENTANG KAMI</a></li>
                 </ul>
               </div>
             </div>
