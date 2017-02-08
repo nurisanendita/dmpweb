f3eaf921ac35dbf6b2c0c258358a4c4b31764aef
nuritharra <nuri.tharra.s@mail.ugm.ac.id>
2017-02-08 22:09:34 +0700

    commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -196,15 +196,19 @@
       <div class="row row2 container col-md-12 col-lg-12">
         <span><img style="margin-left:30px; margin-bottom:10px" src="rekorcapaian.png"></span>
         <div>
-          <p style="text-align:right;color:#6A6161;"><b>{{$rekortercapai->hari}} HARI</b></p>
-          <p style="text-align:right;color:#6A6161;"><b>{{$rekortercapai->jam}} JAM</b></p>
+          @foreach($rekortercapai as $rekortercapai_list)
+          <p style="text-align:right;color:#6A6161;"><b> HARI</b></p>
+          <p style="text-align:right;color:#6A6161;"><b>{{$rekortercapai_list->jam}} JAM</b></p>
+          @endforeach
         </div>
       </div>
       <div class="row container col-md-12 col-lg-12" style="margin-top:20px;">
         <span><img style="margin-left:30px; margin-bottom:10px" src="rekorskrg.png"></span>
         <div>
+          @foreach($rekorskrg as $rekorskrg_list)
           <p style="text-align:right;color:#6A6161;"><b>{{$rekorskrg->hari}} HARI</b></p>
           <p style="text-align:right;color:#6A6161;"><b>{{$rekorskrg->jam}} JAM</b></p>
+          @endforeach
         </div>
       </div>
 
