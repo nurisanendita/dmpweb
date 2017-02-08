c534a4f89c41e2b93158baaf03648f437ab97e3a
nuritharra <nuri.tharra.s@mail.ugm.ac.id>
2017-02-08 13:12:16 +0700

    commit

--- resources/views/dokumentasi/dokumentasi.blade.php
+++ resources/views/dokumentasi/dokumentasi.blade.php
@@ -9,7 +9,7 @@
       @foreach($dokumentasi as $dokumentasi_list)
       <div class="container mySlides col-md-12 col-lg-12" >
         <div class="col-md-3 col-lg-3">
-          <div class="info-box" style="height:500px; font-family:Hind;">
+          <div class="info-box" style="height:500px; font-family:Hind; margin-top:20px;">
             <div class="info-box-content" style="margin-left:20px; margin-right:20px;">
               <span><h5 style="margin-top:20px;"><b>{{ $dokumentasi_list->judul_foto }} </b></h5></span><br>
               <div class="container" style="height:300px; width:220px; font-family:Hind;">
