367049d78f1a8a157fa95f470421487e23107e2c
nuritharra <nuri.tharra.s@mail.ugm.ac.id>
2017-02-08 10:05:38 +0700

    commit

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -32,7 +32,7 @@
                   </div>
               </div>
         </div>
-        <nav class="navbar navbar-inverse">
+        <nav class="navbar navbar-inverse" style="height:500px;">
             <div class="container">
               <div class="navbar-header">
                 <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
