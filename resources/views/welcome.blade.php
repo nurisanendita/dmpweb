3e9c07a0aa79058ae0d7233b5d0f2573af5fda44
nuritharra <nuri.tharra.s@mail.ugm.ac.id>
2017-02-08 18:04:45 +0700

    commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -185,7 +185,7 @@
     <div class="col-md-3 col-lg-3">
       <div class="container mySlides2 col-md-12 col-lg-12" style="margin-left:30px;">
           <div class="row container info-box bg-orange" style="height:200px;">
-            <img src="img_kaltim1.jpg" style="height:200px;width:270px;">
+            <img src="logodmp.png" style="height:200px;width:270px;">
           </div>
       </div>
       <div class="container mySlides2 col-md-12 col-lg-12" style="margin-left:30px;">
